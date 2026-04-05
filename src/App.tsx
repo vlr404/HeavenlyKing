813e433096b74d2a7abe3c694e6673850300169a
vlr404 <valeratitov443@gmail.com>
2026-04-05 23:00:34 +0300

    feat: add cart functionality with Zustand store and integrate cart page
    - Added Zustand store for managing cart state including add, remove, and update item quantities.
    - Created Cart page with layout for displaying items and total summary.
    - Integrated cart icon in header with item count badge.
    - Updated package.json to include Zustand and Sass.
    - Removed unused Template component and styles.
    - Added styles for Cart page and header enhancements.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
 import { Header } from './components/common/Header/Header'
+import Cart from './pages/Cart'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       </div>
     </Router>
